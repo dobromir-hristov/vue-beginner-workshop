7b4bdf3d4dd6dbc97a4d2a7816bb26b269399117
Dobromir Hristov <dobromir92@gmail.com>
2018-12-29 23:59:50 +0200

    feat: add final solution.

---
+++ final/src/components/Members.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="members">
+    <div
+      v-for="member in members"
+      :key="member.id"
+      class="member"
+    >
+      <router-link :to="{ name: 'member', params: { houseId: houseId, memberId: member.id } }">{{ member.name }}</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    members: Array,
+    houseId: String
+  }
+}
+</script>
